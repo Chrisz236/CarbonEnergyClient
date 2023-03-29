0d841773ee89bb23719a1cda3e0e7a579ef3bc81
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2023-03-28 18:12:41 -0700

    Change from setContent -> second screen entrance

--- app/src/main/java/com/example/carbonenergy/MainActivity.kt
+++ app/src/main/java/com/example/carbonenergy/MainActivity.kt
@@ -1,25 +1,109 @@
 package com.example.carbonenergy
 
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.compose.setContent
-import com.example.carbonenergy.ui.CarbonEnergyApp
-import com.example.carbonenergy.ui.theme.CarbonEnergyTheme
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private var sensorManager: SensorManager? = null
+
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
         val userId = findViewById<EditText>(R.id.textUserID)
         val submitButton = findViewById<Button>(R.id.buttonSubmit)
         submitButton.setOnClickListener {
-            setContent {
-                CarbonEnergyTheme {
-                    CarbonEnergyApp(userId)
-                }
-            }
+            var intent = Intent(this, Home::class.java)
+            intent.putExtra("user_id", userId.text.toString())
+            startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if(stepSensor == null) {
+            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+//        var progress_circular = findViewById<ProgressBar>(R.id.circularProgressBar)
+
+        if(running) {
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            tv_stepsTaken.text = ("$currentSteps")
+
+//            progress_circular.apply {
+//                progress = currentSteps
+//            }
+        }
+    }
+
+    fun resetSteps() {
+        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+        tv_stepsTaken.setOnClickListener {
+            // This will give a toast message if the user want to reset the steps
+            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
+        }
+
+        tv_stepsTaken.setOnLongClickListener {
+
+            previousTotalSteps = totalSteps
+
+            // When the user will click long tap on the screen,
+            // the steps will be reset to 0
+            tv_stepsTaken.text = 0.toString()
+
+            // This will save the data
+            saveData()
+
+            true
+        }
+    }
+
+    private fun saveData() {
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+
+    private fun loadData() {
+        // In this function we will retrieve data
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+
+        // Log.d is used for debugging purposes
+        Log.d("MainActivity", "$savedNumber")
+
+        previousTotalSteps = savedNumber
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
     }
 }
