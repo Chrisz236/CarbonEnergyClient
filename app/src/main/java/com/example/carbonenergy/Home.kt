bbe2fcd757552e3de9d276724f92570ef7f539a8
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2023-03-28 19:15:58 -0700

    Show user info in new layout

--- app/src/main/java/com/example/carbonenergy/Home.kt
+++ app/src/main/java/com/example/carbonenergy/Home.kt
@@ -1,27 +1,31 @@
 package com.example.carbonenergy
 
+import android.annotation.SuppressLint
+import kotlinx.coroutines.*
 import android.os.Bundle
-import androidx.activity.compose.setContent
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.carbonenergy.ui.CarbonEnergyApp
-import com.example.carbonenergy.ui.screens.CarbonEnergyViewModel
-import com.example.carbonenergy.ui.theme.CarbonEnergyTheme
+import com.example.carbonenergy.model.User
+import com.example.carbonenergy.network.CarbonEnergyApi
 
 class Home : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu)
         intent = getIntent()
-        val userId = intent.getStringExtra("user_id")
-        val carbonEnergyViewModel: CarbonEnergyViewModel
 
-//        setContent {
-//            CarbonEnergyTheme {
-//                if (userId != null) {
-//                    CarbonEnergyApp(userId)
-//                }
-//            }
-//        }
+        val userId = intent.getStringExtra("user_id").toString()
+        var user = User("null", -1)
+
+        runBlocking {
+            val job = launch {
+                user = CarbonEnergyApi.retrofitService.getUser(userId)
+            }
+            job.join()
+        }
+
+        val test = findViewById<TextView>(R.id.textGreeting)
+        test.setText("Hello, ${user.user_id}\nYou have ${user.energy} points!")
     }
 }
